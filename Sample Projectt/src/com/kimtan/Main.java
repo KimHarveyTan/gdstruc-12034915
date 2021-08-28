f0f78796b288437c6f5321231f93ef7a7d4b81a5
Kim Harvey Tan <kimharvey.tan@benilde.edu.ph>
2021-08-29 04:11:03 +0800

    Added Sample Project

---
+++ Sample Projectt/src/com/kimtan/Main.java
@@ -0,0 +1,21 @@
+package com.kimtan;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("What is your name?");
+        String name = scanner.nextLine();
+
+        System.out.println("Hi " + name + "!");
+
+        System.out.println("What is your age?");
+        String age = scanner.nextLine();
+
+        System.out.println("You are " + age + " years old!");
+    }
+}
